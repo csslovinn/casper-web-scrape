b4e4b8689f32846ed04b59b84059b45a94abc1a4
Lisa Burgess <lisa@raineverywhere.com>
2014-11-21 15:43:23 -0500

    added function to loop through both vegetable and plant URLs

--- scrape.js
+++ scrape.js
@@ -18,55 +18,56 @@ function Item (name, zones, sun_type, soil_type, ph, instructions) {
     	this.instructions = instructions;
 }
 
-casper.start('http://www.almanac.com/plants/type/vegetable'); 
+ var links = [
+ 'http://www.almanac.com/plants/type/vegetable',
+ 'http://www.almanac.com/plants/type/plant'
+ ];
 
-//give page time to load, then click object link
-casper.wait(3000, function () {
-	//this.click('.field-content a');
-	var type_selector = '.views-field-title a[href*="plant"]';
-	var type_info = this.getElementsInfo(type_selector);
-	var types = [];
-	for (var i = 0; i < type_info.length; i++) {
-    	types.push('http://www.almanac.com' + type_info[i].attributes.href);
-    }
-    utils.dump(types);
+ casper.start().each(links, function(self, link) {
+    self.thenOpen(link, function() {
+        casper.wait (3000, function() {       
+            //grab URLs for each vegetable profile page listed
+        	var type_selector = '.views-field-title a[href*="plant"]';
+        	var type_info = this.getElementsInfo(type_selector);
+        	var types = [];
+        	for (var i = 0; i < type_info.length; i++) {
+            	types.push('http://www.almanac.com' + type_info[i].attributes.href);
+            }
+            //utils.dump(types);
 
-});
+            //open each link in array, then grab data and store in a new Item
+            casper.start().each(types, function(self, link) {
+                self.thenOpen(link, function() {
+                     //name
+                    var name = this.fetchText('h1');
+                    //create an array of the zones
+                    var zone_selector = 'a[href*="hardiness-zone"]';  
+                    var zone_info = this.getElementsInfo(zone_selector);
+                    var zones = [];
+                    for (var i = 0; i < zone_info.length; i++) {
+                        var zone = parseInt(zone_info[i].text);
+                        zones.push(zone);//use this for numeric values in array
+                    }
+                    //sun and soil info
+                    var sun_type = this.fetchText('a[href*="sun"]');
+                    var soil_type = this.fetchText('a[href^="/plants/soil/"]');
+                    var ph = this.fetchText('a[href*="soilph"]');
+                    //select planting info
+                    var instr_selector = '#article-body > h2:first-of-type + p + ul > li';
+                    var instr_info = this.getElementsInfo(instr_selector);
+                    var instructions = [];
+                    for (var i = 0; i < instr_info.length; i++) {
+                        instructions.push(instr_info[i].text);
+                    }
 
-//test url opened by object link
-casper.then(function() {
-    this.echo('clicked link, new location is ' + this.getCurrentUrl());
-});
-
-//grab data from object profile page
-casper.then(function() {
-
-    //name
-    var name = this.fetchText('h1');
-    //create an array of the zones
-    var zone_selector = 'a[href*="hardiness-zone"]';  
-    var zone_info = this.getElementsInfo(zone_selector);
-    var zones = [];
-    for (var i = 0; i < zone_info.length; i++) {
-    	zones.push(zone_info[i].text);
-    	/* var zone = parseInt(zone_info[i].text);
-    	zones.push(zone); */ //use this for numeric values in array
-    }
-	//sun and soil info
-	var sun_type = this.fetchText('a[href*="sun"]');
-	var soil_type = this.fetchText('a[href^="/plants/soil/"]');
-	var ph = this.fetchText('a[href*="soilph"]');
-	//select planting info
-	var instr_selector = '#article-body > h2:first-of-type + p + ul > li';
-	var instr_info = this.getElementsInfo(instr_selector);
-	var instructions = [];
-	for (var i = 0; i < instr_info.length; i++) {
-    	instructions.push(instr_info[i].text);
-    }
-
-    //create new vegetable
-    var vegetable = new Item (name, zones, sun_type, soil_type, ph, instructions);
-    utils.dump(vegetable);
+                    //create new vegetable
+                    var plant = new Item (name, zones, sun_type, soil_type, ph, instructions);
+                    utils.dump(plant);
+                    return (plant);
+                });
+            });
+        });
+    });
 });
 
 casper.then(function () {
